0d56499456eefa037e2699dca723b464cf960dad
owen-d <ow.diehl@gmail.com>
2014-10-29 16:08:19 -0700

    Function Q and stack working

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,29 @@
 var makeQueue = function(){
   var someInstance = {};
-
+  var counter = 0;
+  var position = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function(){
+    var tempValue = storage[position];
+    delete storage[position];
+    if (counter > position) {
+      position++;
+    }
+    return tempValue;
+
   };
 
   someInstance.size = function(){
+    return counter-position;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
   // Implement the methods below
   someInstance.push = function(value){
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function(){
+    if (counter > 0) {
+      counter--;
+    }
+    var temp = storage[counter];
+    delete storage[counter];
+    return temp;
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
