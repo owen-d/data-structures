b74fcc909cec41d4b1dfd3cd34ac0be5ef537ed4
owen-d <ow.diehl@gmail.com>
2014-10-30 11:34:32 -0700

    prototypal functions working

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try no t not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+  obj.count = 0;
+  obj.position = 0;
+  return obj;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
+queueMethods.dequeue = function(){
+
+  var tempValue = this.storage[this.position];
+  delete this.storage[this.position];
+  if (this.size()>0) {
+    this.position++;
+  }
+  return tempValue;
+};
+queueMethods.size = function(){
+  return this.count-this.position;
+};
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.count = 0;
+  obj.storage = {};
+  return obj;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+stackMethods.pop = function(){
+  if (this.count>0){
+    this.count--;
+  }
+  var tempValue = this.storage[this.count];
+  delete this.storage[this.count];
+  return tempValue;
+};
+stackMethods.size = function(){
+  return this.count;
+};
