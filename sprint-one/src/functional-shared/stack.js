360b3dde3fd1f6f8e97cdcb1abff997debdf24c6
owen-d <ow.diehl@gmail.com>
2014-10-30 10:51:08 -0700

    func-shared stack and Q working

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var obj = {};
+obj.count = 0;
+obj.storage = {};
+_.extend(obj, stackMethods);
+return obj;
 };
 
 var stackMethods = {};
-
+stackMethods.push = function(item){
+  this.storage[this.count] = item;
+  this.count++;
+};
+stackMethods.pop = function(){
+  if (this.count > 0) {
+    this.count--;
+  }
+  var tempValue = this.storage[this.count];
+  delete this.storage[this.count];
+  return tempValue;
+};
+stackMethods.size = function(){
+  return this.count;
+};
 
