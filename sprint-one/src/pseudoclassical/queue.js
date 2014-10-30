681bf1c408e3f8a436d6937f9b389559ffc876d5
owen-d <ow.diehl@gmail.com>
2014-10-30 13:55:34 -0700

    Pseudoclassical stack/q working

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.position = 0;
+  this.count = 0;
+
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.count] = value;
+  this.count++
+};
+Queue.prototype.dequeue = function(){
+  var tempValue = this.storage[this.position];
+  delete this.storage[this.position];
+  if (this.size()>0) {
+    this.position++;
+  }
+  return tempValue;
+};
+Queue.prototype.size = function(){
+  return this.count-this.position;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
-
+Stack.prototype.push = function(item){
+  this.storage[this.count] = item;
+  this.count++;
+};
+Stack.prototype.pop = function(){
+  if (this.count > 0) {
+    this.count--;
+  }
+  var tempValue = this.storage[this.count];
+  delete this.storage[this.count];
+  return tempValue;
+};
+Stack.prototype.size = function(){
+  return this.count;
+};
